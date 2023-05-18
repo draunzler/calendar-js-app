9572b55c73f3e4a2179e6911e789bd3b4e0ee929
Draunzler <shadymeister2017@gmail.com>
2023-05-19 01:14:06 +0530

    js update

--- script.js
+++ script.js
@@ -20,3 +20,35 @@ const months = [
     "November",
     "December",
 ];
+let selectedDate;
+const renderCalendar = () => {
+  let firstDayofMonth = new Date(currYear, currMonth, 1).getDay(), // getting first day of month
+    lastDateofMonth = new Date(currYear, currMonth + 1, 0).getDate(), // getting last date of month
+    lastDayofMonth = new Date(currYear, currMonth, lastDateofMonth).getDay(), // getting last day of month
+    lastDateofLastMonth = new Date(currYear, currMonth, 0).getDate(); // getting last date of previous month
+  let liTag = "";
+  for (let i = firstDayofMonth; i > 0; i--) {
+    liTag += `<li class="inactive">${lastDateofLastMonth - i + 1}</li>`;
+  }
+  for (let i = 1; i <= lastDateofMonth; i++) {
+    let isToday = i === date.getDate() && currMonth === new Date().getMonth() && currYear === new Date().getFullYear() ? "active" : "";
+    liTag += `<li class="${isToday}">${i}</li>`;
+  }
+  for (let i = lastDayofMonth; i < 6; i++) {
+    liTag += `<li class="inactive">${i - lastDayofMonth + 1}</li>`;
+  }
+  currentDate.innerText = `${months[currMonth]} ${currYear}`; // passing current mon and yr as currentDate text
+  daysTag.innerHTML = liTag;
+
+  const liTags = document.querySelectorAll(".days li");
+  liTags.forEach((li) => {
+    li.addEventListener("click", () => {
+      selectedDate = li;
+      liTags.forEach((tag) => {
+        tag.classList.remove("active");
+      });
+      selectedDate.classList.add("active");
+      console.log(selectedDate);
+    });
+  });
+};
